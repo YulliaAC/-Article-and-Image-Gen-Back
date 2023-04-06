056fcf52250c1336bf278ee2febb69828277a895
Yulia Semenyuk <semenyukyp@gmail.com>
2023-04-07 00:02:10 +0300

    article model

--- controllers/editArticles.js
+++ controllers/editArticles.js
@@ -1,5 +1,6 @@
 const ctrlWrapper = require('../helpers');
 const { Configuration, OpenAIApi } = require("openai");
+const {Article} = require('../models/article');
 
 const configuration = new Configuration({
     apiKey: process.env.OPEN_AI_KEY,
--- models/article.js
+++ models/article.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+const {handleMongooseError} = require("../helpers");
+
+const articleSchema = new mongoose.Schema (
+    {
+        title: {
+            type: String,
+            required: [true, 'Set title for article'],
+        },
+        body: {
+            type: String,
+            required: [true, 'Set body for article'],
+        }
+    },
+    {versionKey: false}
+);
+
+articleSchema.post("save", handleMongooseError);
+
+const Article = mongoose.model ('articles', articleSchema);
+
+module.exports = {
+  Article,
+};
